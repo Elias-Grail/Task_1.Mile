aabaa6aa989d7ad2dc15ae539f9c6f2c3abceb0e
Elias Graal <shadowgt@mail.ru>
2022-12-06 00:06:30 +0300

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int ticketPrice = 1500;
+        int bonus = ticketPrice / 20;
+        int mile = bonus * 1;
+
+        System.out.println("Вы накопили: " + mile + " миль.");
+
+    }
+}
